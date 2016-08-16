0c09f88331e94b067b25c22448e4b97be5aff691
maxemiliang <maxemiliang@gmail.com>
2016-08-16 09:01:10 +0300

    initalized nodejs project and added hapi

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 1337 });
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('index.html');
+        }
+    });
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+
+        console.log('Server running at:', server.info.uri);
+    });
+});
