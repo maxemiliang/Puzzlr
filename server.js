0b7c855eb77eec50da8c212f9b75bd16354e21d2
maxemiliang <maxemiliang@gmail.com>
2016-08-16 09:25:24 +0300

    updated hapi config and added working css and js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Path = require('path');
 
-const server = new Hapi.Server();
-server.connection({ port: 1337 });
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'views')
+      }
+    }
+  }
+});
+
+server.connection({ 
+  port: 1337 
+});
 
 server.register(require('inert'), (err) => {
 
@@ -11,20 +23,40 @@ server.register(require('inert'), (err) => {
         throw err;
     }
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-            reply.file('views/index.html');
+    server.route({  
+      method: 'GET',
+      path: '/js/{file*}',
+      handler: {
+        directory: { 
+          path: 'js'
         }
+      }
+    });
+
+    server.route({  
+      method: 'GET',
+      path: '/css/{file*}',
+      handler: {
+        directory: { 
+          path: 'css'
+        }
+      }
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+          reply.file('index.html');
+      }
     });
 
     server.start((err) => {
-
+      
         if (err) {
             throw err;
         }
-
+        
         console.log('Server running at:', server.info.uri);
     });
 });
